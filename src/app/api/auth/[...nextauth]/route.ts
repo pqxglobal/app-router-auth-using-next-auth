d0355985e43a0bb4fcaf33c955075da15d19c0e7
pqxglobal <l.r.slegt@gmail.com>
2023-09-08 13:58:22 +0200

    🐛 export AuthOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { authOptions } from '@/app/utils/authOptions'
 import NextAuth from 'next-auth/next'
 
-const handler = NextAuth(authOptions)
+export const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
