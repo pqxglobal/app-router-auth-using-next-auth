eadf3bb54d2c1e6b2219e97c79f0ad666b3f775f
pqxglobal <l.r.slegt@gmail.com>
2023-09-08 13:55:30 +0200

    🐛 Add utils/authOptions.ts

---
+++ src/app/utils/authOptions.ts
@@ -0,0 +1,15 @@
+import { NextAuthOptions } from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? '',
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+}
